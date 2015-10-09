62a76263bb8ec3a791a4f10a28f26e14652fd9b6
ainahcoleen <ainahcoleenramirez@gmail.com>
2015-10-09 21:20:17 +0800

    remove keyword content
    remove keyword content

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -15,8 +15,7 @@
 		<title>Kalugdan Garden Resort</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="keywords" content="Blue Water Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-		Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+		<meta name="keywords" content="Kalugdan Garden Resort" />
 		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<link href="{{app('customURL')}}css/bootstrap.css" rel='stylesheet' type='text/css' />
 		<!--<link href="{{app('customURL')}}css/bootstrap.min.css" rel='stylesheet' type='text/css' />-->
